e03eb84e05f226bee5f99d76adf375b4c4e622bc
Seth Holloway <smholloway@gmail.com>
2014-11-09 22:02:14 -0600

    Scraping the pfsrd site

--- snippets.js
+++ snippets.js
@@ -13,3 +13,22 @@ myObj.myrows = myRows;
 console.table(myRows)
 
 copy(myRows);
+
+
+
+var myRows = [];
+var $headers = document.querySelectorAll("#sites-canvas-main-content > div > table > tbody > tr > td.sites-layout-tile.sites-tile-name-content-1 > div > div:nth-child(2) > table th");
+var $rows = document.querySelectorAll("#sites-canvas-main-content > div > table > tbody > tr > td.sites-layout-tile.sites-tile-name-content-1 > div > div:nth-child(2) > table tbody tr");
+for (var i = 3, l = $rows.length, index = 0; i < l; i++, index++) {
+  $cells = $rows[i].cells;
+  myRows[index] = {};
+  for (var j = 0; j < $cells.length; j++) {
+    myRows[index][$headers[j].innerText] = $cells[j].innerText;
+  }
+}
+
+var myObj = {};
+myObj.myrows = myRows;
+console.table(myRows)
+
+copy(myRows);
